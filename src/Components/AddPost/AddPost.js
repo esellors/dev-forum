a767b02d4cb450ecb5de82535b5014e46dac5619
Eric <github@esellors.com>
2019-09-19 14:16:32 -0500

    components set up

---
+++ src/Components/AddPost/AddPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AddPost.css';
+
+function AddPost() {
+   return (
+      <div id='AddPost'>
+         <h1>AddPost</h1>
+      </div>
+   );
+}
+
+export default AddPost;
