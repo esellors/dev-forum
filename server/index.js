37f1c183915b4dba78df7b27fc9fa2ccf7da3540
Eric <github@esellors.com>
2019-09-19 09:56:32 -0500

    server set up

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const {SERVER_PORT} = process.env;
+
+// auth endpoints
+app.get('/auth/user');
+app.post('/auth/register');
+app.post('/auth/login');
+app.post('/auth/logout');
+
+// posts endpoints
+app.get('/api/forums');
+app.get('/api/posts/:topic');
+app.post('/api/posts/:topic');
+app.delete('/api/posts/:id')
 
 
-
-
-
-
-
-app.listen
+app.listen(SERVER_PORT, () => console.log(`Server listening on ${SERVER_PORT}`))
