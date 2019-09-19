7ba574e8043c1528ff657ee1874c085801f0485c
Eric <github@esellors.com>
2019-09-19 12:03:22 -0500

    auth controller set up

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,14 +1,65 @@
+const bcrypt = require('bcryptjs');
+
 module.exports = {
    getUser: (req, res) => {
-
+      if (req.session.user) {
+         res.status(200).json(req.session.user);
+      } else {
+         res.sendStatus(401)
+      }
    },
-   register: (req, res) => {
+   register: async (req, res) => {
+      const {username, password, firstName} = req.body;
+      const db = req.app.get('db');
 
+      const foundUser = await db.auth.checkForUsername(username);
+
+      if (foundUser[0]) {
+         res.status(409).json("Username Taken")
+      } else {
+
+         const salt = bcrypt.genSaltSync(10);
+         const hash = bcrypt.hashSync(password, salt)
+
+         const newUser = await db.auth.registerUser(firstName, username, hash);
+
+         req.session.user = {
+            user_id: newUser[0].user_id,
+            username: newUser[0].username,
+            firstName: newUser[0].first_name
+         };
+
+         res.status(200).json(req.session.user);
+      }
    },
-   login: (req, res) => {
+   login: async (req, res) => {
+      const {username, password} = req.body;
+      const db = req.app.get('db');
 
+      const foundUser = await db.auth.checkForUsername(username);
+
+      if (!foundUser[0]) {
+         res.status(403).json("Username or Password incorrect")
+      } else {
+
+         const isAuthenticated = bcrypt.compareSync(password, foundUser[0].hash)
+
+         if (!isAuthenticated) {
+            res.status(403).json("Username or Password incorrect")
+         } else {
+
+            req.session.user = {
+               user_id: foundUser[0].user_id,
+               username: foundUser[0].username,
+               firstName: foundUser[0].first_name
+            };
+
+            res.status(200).json(req.session.user);
+         }
+      }
    },
    logout: (req, res) => {
-      
+      req.session.destroy();
+      res.sendStatus(200);
    }
 }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const massive = require('massive');
+const session = require('express-session');
 const authController = require('./controllers/authController');
 const postsController = require('./controllers/postsController');
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
+
+app.use(express.json());
+
+app.use(session({
+   secret: SESSION_SECRET,
+   resave: false,
+   saveUninitialized: true,
+   cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7
+   }
+}));
+
+massive(CONNECTION_STRING).then(db => {
+   app.set('db', db);
+   console.log('db connected')
+});
 
 // auth endpoints
 app.get('/auth/user', authController.getUser);
@@ -12,8 +30,8 @@ app.post('/auth/login', authController.login);
 app.post('/auth/logout', authController.logout);
 
 // posts endpoints
-app.get('/api/forums', postsController.getForums);
-app.get('/api/posts/:topic', postsController.topics);
+app.get('/api/topics', postsController.topics);
+app.get('/api/posts/:topic', postsController.posts);
 app.post('/api/posts/:topic', postsController.addPost);
 app.delete('/api/posts/:id', postsController.deletePost);
 
